5e88c7f4a3ce882c42ff8176ce3ae00ace7da45d
Nickolaus Gotsiridze <ngotsiridze@accumtech.com>
2025-02-06 22:45:37 -0500

    Investments component now displays a skeleton on fallback

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,14 @@
+import { Suspense } from "react";
 import Investments from "./investments";
+import { RevenueChartSkeleton } from "../ui/skeletons";
 
 export default function Page() {
   return (
     <>
-      <p>I did the digital dash</p> <Investments />
+      <p>I did the digital dash</p>
+      <Suspense fallback={<RevenueChartSkeleton />}>
+        <Investments />
+      </Suspense>
     </>
   );
 }
