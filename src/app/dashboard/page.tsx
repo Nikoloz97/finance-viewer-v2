cd91651712e916574afd4ff140ba02a93151abae
Nikoloz97 <nick.gotsy@gmail.com>
2025-02-13 19:34:31 -0500

    test

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,6 +5,7 @@ import { RevenueChartSkeleton } from "../ui/skeletons";
 export default function Page() {
   return (
     <>
+      // Test comment
       <p>I did the digital dash</p>
       <Suspense fallback={<RevenueChartSkeleton />}>
         <Investments />
