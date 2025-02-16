c407f5705a35d342904eb6bee7ed48c8a2bbb100
Nikoloz97 <nick.gotsy@gmail.com>
2025-02-16 12:49:59 -0500

    Created outline of sidebar; added sidebar css; added custom vars to globals css

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,11 +4,19 @@ import { RevenueChartSkeleton } from "../ui/skeletons";
 
 export default function Dashboard() {
   return (
-    <>
-      <p>Welcome to the dashboard</p>
-      <Suspense fallback={<RevenueChartSkeleton />}>
+    <div className="dashboard-container">
+      <div className="networth-profile-container">
+        <div className="networth-container">{/* <Networth /> */}</div>
+        <div className="profile-container">{/* <Profile /> */}</div>
+      </div>
+      <div className="recent-activity-investments-summary-container">
+        {/* <RecentActivity /> */}
+        <div className="investments-summary-container"> Investments</div>
+      </div>
+      {/* TODO: move this out eventually */}
+      {/* <Suspense fallback={<RevenueChartSkeleton />}>
         <Investments />
-      </Suspense>
-    </>
+      </Suspense> */}
+    </div>
   );
 }
--- src/app/ui/sidebar.tsx
+++ src/app/ui/sidebar.tsx
@@ -1,3 +1,26 @@
+import "./sidebar.css";
+import Link from "next/link";
+
 export default function Sidebar() {
-  return <div>This is a sidebar</div>;
+  return (
+    <div className="navbar-container">
+      <div className="navbar">
+        <h2 style={{ marginTop: "10px" }}>Finance Viewer</h2>
+        <div className="navbar-options">
+          <Link href={"/"}>Dashboard</Link>
+          <Link href={"/budget"}>Budget</Link>
+          <Link href={"/allocation"}>Allocation</Link>
+          <Link href={"/debt"}>Debt</Link>
+          <Link href={"/investments"}>Investments</Link>
+          {/* TODO: set this up once useContext is set up */}
+          {/* {user ? (
+        <Link href={"/profile"}>Profile</Link>
+      ) : (
+        <Link href={"user/login"}>Login</Link>
+      )} */}
+          <Link href={"/settings"}>Settings</Link>
+        </div>
+      </div>
+    </div>
+  );
 }
