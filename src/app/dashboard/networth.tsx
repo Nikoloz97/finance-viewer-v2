985660ed3316839142f2594fcca2776f49599f61
Nikoloz97 <nick.gotsy@gmail.com>
2025-02-16 16:39:25 -0500

    Created networth, profile, recent activity, and investments sections for dashboard

---
+++ src/app/dashboard/networth.tsx
@@ -0,0 +1,3 @@
+export default function Networth() {
+  return <div>Networth</div>;
+}
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,16 +1,26 @@
 import { Suspense } from "react";
 import Investments from "./investments";
 import { RevenueChartSkeleton } from "../ui/skeletons";
+import "./dashboard.css";
+import Networth from "./networth";
+import Profile from "./profile";
+import RecentActivity from "./recentactivity";
 
 export default function Dashboard() {
   return (
     <div className="dashboard-container">
       <div className="networth-profile-container">
-        <div className="networth-container">{/* <Networth /> */}</div>
-        <div className="profile-container">{/* <Profile /> */}</div>
+        <div className="networth-container">
+          <Networth />
+        </div>
+        <div className="profile-container">
+          <Profile />
+        </div>
       </div>
       <div className="recent-activity-investments-summary-container">
-        {/* <RecentActivity /> */}
+        <div className="recent-activity-container">
+          <RecentActivity />
+        </div>
         <div className="investments-summary-container"> Investments</div>
       </div>
       {/* TODO: move this out eventually */}
